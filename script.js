e19edba38006199c18ebf6a9bc8cba039f8c9252
Justin Lee <dwlee1399@gmail.com>
2023-07-21 23:35:55 +0900

    Made it possible the calculate the final input and display it

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 let operators = {
     "+": (a,b) => a + b,
     "-": (a,b) => a - b,
-    "× ": (a,b) => a * b,
+    "×": (a,b) => a * b,
     "÷": (a,b) => a / b,
-    "^": (a,b) => Math.pow(a,b),
-    ".": "decimal point"
-
+    "^": (a,b) => Math.pow(a,b)
 }
 
 let operand1, operator, operand2;
@@ -18,6 +16,8 @@ function operate(a, op, b) {
 const screenDisplayPart = document.querySelector('#display-part');
 const resultPart = document.querySelector('#result-part');
 
+//Making the number buttons work
+
 const numberButtons = document.querySelectorAll('.number-button');
 
 numberButtons.forEach((button) => {
@@ -28,6 +28,8 @@ numberButtons.forEach((button) => {
             screenDisplayPart.textContent += button.textContent;
     })
 })
+
+//Making the operator buttons work
 
 const operatorButtons = document.querySelectorAll('.operator-button');
 
@@ -42,6 +44,8 @@ operatorButtons.forEach((button) => {
     })
 })
 
+//Making the exponential button work
+
 const exponentialButton = document.querySelector('#exponential');
 
 exponentialButton.addEventListener('click', function(){
@@ -53,11 +57,16 @@ exponentialButton.addEventListener('click', function(){
         screenDisplayPart.textContent += '^';
 })
 
+//Making the AC button work
+
 const allClearButton = document.querySelector('#all-clear');
 
 allClearButton.addEventListener('click', function(){
     screenDisplayPart.textContent = '0';
+    resultPart.textContent = '0';
 })
+
+//Making the clear button work
 
 const clearButton = document.querySelector('#clear');
 
@@ -67,3 +76,92 @@ clearButton.addEventListener('click', function(){
     screenDisplayPart.textContent = expressionToArray.join('');
     
 })
+
+
+//Dealing with the displayed expression and getting the result
+
+function combineDigits(arr) {
+    const result = [];
+    let currentNumber = "";
+
+    for (const item of arr) {
+        if (!isNaN(Number(item)))
+            currentNumber += item;
+        else {
+            if (currentNumber !== "") {
+                result.push(currentNumber);
+                currentNumber = "";
+            }
+            result.push(item);
+        }
+    }
+
+    if (currentNumber !== "")
+        result.push(currentNumber);
+    return result;
+}
+
+function infixToPostfix(tokens) {
+    const precedence = {
+        "+": 1,
+        "-": 1,
+        "×": 2,
+        "÷": 2,
+        "^": 3
+    }
+
+    const outputQueue = [];
+    const operatorStack = [];
+
+    for (const token of tokens) {
+        if (!isNaN(Number(token))) {
+            outputQueue.push(token);
+        }
+        else if (token in precedence) {
+            while (operatorStack.length > 0 && precedence[operatorStack[operatorStack.length - 1]] >= precedence[token]) {
+                outputQueue.push(operatorStack.pop());
+            }
+            operatorStack.push(token);
+        }
+    }
+
+    while (operatorStack.length > 0)
+        outputQueue.push(operatorStack.pop());
+
+    return outputQueue;
+}
+
+function evaluatePostfix(tokens) {
+    while(tokens.length > 1) {
+        for (let token of tokens) {
+            if (operators[token]) {
+                let indexOfToken = tokens.indexOf(token);
+                let a = +tokens[indexOfToken-2];
+                let b = +tokens[indexOfToken-1];
+                
+                let operatedNum = operate(a,token,b);
+                tokens.splice(indexOfToken-2,3,operatedNum);
+                break;
+            }
+        }
+    }
+    
+    return tokens[0];
+} 
+
+
+
+function calculate() {
+    let displayedExpression = screenDisplayPart.textContent;
+    let expressionTokenized = combineDigits(displayedExpression.split(''));
+
+    if (isNaN(expressionTokenized[expressionTokenized.length-1]))
+        return;
+
+    let expressionToPostFix = infixToPostfix(expressionTokenized);
+    let finalResult = evaluatePostfix(expressionToPostFix);
+    resultPart.textContent = finalResult;
+}
+
+const equalsButton = document.querySelector('#equals');
+equalsButton.addEventListener("click", calculate);
